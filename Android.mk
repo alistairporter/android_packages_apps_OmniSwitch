f0f8a6046254f45411b27af86912eecf9aba3773
Alistair Porter <alistair_porter@outlook.com>
2018-09-11 19:40:49 +0100

    Added Permission File

--- Android.mk
+++ Android.mk
@@ -17,10 +17,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-org.omnirom.omniswitcher.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := OmniSwitch
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_MIN_SDK_VERSION := 24
+LOCAL_REQUIRED_MODULES := privapp-permissions-org.omnirom.omniswitcher.xml
 include $(BUILD_PACKAGE)
